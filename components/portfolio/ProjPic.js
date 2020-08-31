79a3a26f12819c369acf45ebb4b407c3d47ba8da
Expedito Andrade <expeditojazz@gmail.com>
2020-08-31 12:22:04 -0300

    add new car for portfolio

--- components/portfolio/ProjPic.js
+++ components/portfolio/ProjPic.js
@@ -7,12 +7,9 @@ const StyledRight = styled.div`
   flex-direction: column;
   align-content: center;
   justify-content: space-between;
-  align-items: center;
   height: 100%;
-  a {
 
     display: flex;
-  }
 
   .icon {
     color: #222629;
@@ -20,93 +17,11 @@ const StyledRight = styled.div`
     margin-right: 5px;
   }
 
-  .iconsProj {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-  }
-
   img {
     flex-grow: 2;
-    border-radius: 30%;
-    width: 70%;
+    width: 100%;
     margin: 0 auto;
   }
-
-  p {
-    margin: 0;
-  }
-
-    
-.containerss {
-  position: relative;
-  background: #222629;
-  height: 60%;
-  width: 50%;
-  padding: 0;
-}
-
-.date {
-    position: absolute;
-    left: 11%;
-    bottom: -7px;
-    color: white;
-    font-weight: 600;
-    font-size: 18px;
-}
-
-.wave {
-  position: absolute;
-  height: 40%;
-  width: 100%;
-  background: #222629;
-  bottom: 0;
-}
-
-.wave::before, .wave::after {
-  content: "";
-  display: block;
-  position: absolute;
-  border-radius: 100% 50%;
-}
-
-.wave::before {
-  width: 55%;
-  height: 109%;
-  background-color:#f5fafa ;
-  right: -1.5%;
-  top: 60%;
-}
-.wave::after {
-  width: 55%;
-  height: 100%;
-  background-color: #222629;
-  left: 0;
-  top: 40%;
-}
-.bCont {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-  align-items: flex-end
-}
-.bottom {
-    box-sizing: border-box;
-    min-width: 0px;
-    
-    background-color: #222629;
-    color: white;
-    display: inline;
-    font-size: 14px;
-    font-weight: 600;
-    text-transform: uppercase;
-    right: 0px;
-    bottom: 0px;
-    clip-path: polygon(20px 0%, 100% 0%, 100% 100%, 0% 100%);
-    margin: 0px;
-    padding: 10px 10px 10px 20px;
-    height: 70%;
-}
 
 `
 
@@ -114,35 +29,11 @@ const StyledRight = styled.div`
 const ProjPic = ({image, stack}) => {
   return (
     <StyledRight className="item">
-      <div className="iconsProj">
-        <div className="containerss">
-          <div className="wave">
-        <p className="date">
-          2020
-        </p>
 
-          </div>
-        </div>
-        <div className="fonts icons">
-        <motion.span className="icon fa-github"
-        whileHover={{color: '#e27689'}} transition={{stype: 'spring', stiffness: 300}}
-        ></motion.span>
-        <motion.span 
-        whileHover={{color: '#e27689'}}
-        className="icon fa-globe"></motion.span>
-        </div>
-      </div>
       <a href="/#" className="image fit">
         <img src={image} alt=""/>
       
       </a>
-      <div className="bCont">
-
-            <div className="bottom">
-  <p>{stack}</p>
-            </div>
-      </div>
-
     </StyledRight>
   )
 }
--- components/portfolio/Project.js
+++ components/portfolio/Project.js
@@ -5,23 +5,28 @@ import { motion } from 'framer-motion';
 
 const StyledProject = styled.div`
   display: flex;
-  height: 100%; 
+  height: fit-content; 
+  margin: 0; 
+  
+  .icons {
+    span {
+      margin: 0 2rem;
+    }
+  } 
   .one {
     background: white;
-  }
+   }
   
   .a {
     display: flex;
   }
-
-  .h100 {
-    height: 100%;
-  }
   .item {
     width: 50%;
+    margin: 0;
   }
   .title {
-    font-size: 18px;
+    font-size: 2rem;
+    font-weight: bold;
   }
   .subtitle {
     font-size: 14px;
@@ -32,9 +37,10 @@ const Project = ({title, stack, subtitle, image}) => {
   console.log(image)
   return (
 
-    <StyledProject  className="col-6 col-12-mobile">
-      <motion.div 
-      whileHover={{scale: 1.07, translateY: -20, boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 8px'}}>
+    <StyledProject  className="col-12">
+          <motion.div 
+         whileHover={{scale: 1.07, translateY: -20, boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 8px'}}>
+     
 
         <div className="a">
 
@@ -48,7 +54,23 @@ const Project = ({title, stack, subtitle, image}) => {
       <header>
         <h3 className='subtitle'>{subtitle}</h3>
       </header>
+      <div className="iconsProj">
+        <div className="containerss">
+          <div className="wave">
+          </div>
+        </div>
+        <div className="fonts icons">
+        <motion.span className="icon fa-github"
+        whileHover={{color: '#e27689'}} transition={{stype: 'spring', stiffness: 300}}
+        ></motion.span>
+        <motion.span 
+        whileHover={{color: '#e27689'}}
+        className="icon fa-globe"></motion.span>
+        </div>
+      </div> 
     </article>
+
+
     <ProjPic image={image} stack={stack} />
         </div>
       </motion.div>
