9a7f3d73012e5a97a35b4fa4310e76878e34c649
Expedito Andrade <expeditojazz@gmail.com>
2020-09-02 06:13:32 -0300

    add proj change

--- components/portfolio/Project.js
+++ components/portfolio/Project.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import ProjPic from './ProjPic'
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { motion, AnimatePresence, useViewportScroll } from 'framer-motion';
-import { useState, useEffect, useRef } from 'react';
-
+import ProjPic from './ProjPic';
 
 const StyledProject = styled.div`
   display: flex;
-  height: fit-content; 
+  height: fit-content;
   margin: 0;
   .opacity-0 {
     opacity: 0;
@@ -16,10 +14,10 @@ const StyledProject = styled.div`
     span {
       margin: 0 2rem;
     }
-  } 
+  }
   .one {
     background: white;
-   }
+  }
   a {
     border: none;
     outline: none;
@@ -28,10 +26,9 @@ const StyledProject = styled.div`
       outline: none;
     }
   }
-  
+
   .a {
     display: flex;
-
   }
   .item {
     width: 50%;
@@ -44,73 +41,81 @@ const StyledProject = styled.div`
   .subtitle {
     font-size: 14px;
   }
-  
-`
-const Project = ({title, stack, subtitle, image, githubUrl, projectUrl}) => {
-  const { scrollYProgress } = useViewportScroll()
-  const [show, setShow] = useState(false)
-  const [lastY, setLastY] = useState(false)
+`;
+const Project = ({
+  title,
+  stack,
+  subtitle,
+  image,
+  githubUrl,
+  projectUrl,
+  id,
+}) => {
+  const { scrollYProgress } = useViewportScroll();
+  const [show, setShow] = useState(false);
+  const [lastY, setLastY] = useState(false);
   const card = useRef();
   useEffect(() => {
-    if (show) console.log('here')
+    if (show) console.log('here');
     const handleScroll = () => {
       const yPos = window.scrollY;
       const cardPos = card.current.getBoundingClientRect().y;
-      setShow(yPos > cardPos - 100)
-
-    }
+      setShow(yPos > cardPos - 100);
+    };
     window.addEventListener('scroll', handleScroll, false);
     return () => {
       window.removeEventListener('scroll', handleScroll, false);
-    }
-  })
+    };
+  });
 
   return (
-
-    <StyledProject  className={`col-12 ${show ? 'opacity-1': 'opacity-0'}`} >
-
-          <motion.div 
-         whileHover={{scale: 1.03, translateY: -20, boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 8px'}} transition={{duration: 0.7}}
-         animate={ show ? {opacity: 1} : {opacity: 0}}
-         
-         >
-
-      
-
+    <StyledProject className={`col-12 ${show ? 'opacity-1' : 'opacity-0'}`}>
+      <motion.div
+        whileHover={{
+          scale: 1.03,
+          translateY: -20,
+          boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 8px',
+        }}
+        transition={{ duration: 0.7 }}
+        animate={show ? { opacity: 1 } : { opacity: 0 }}
+      >
         <div className="a" ref={card}>
+          <article className="item one h100">
+            <a href="/#" className="image fit"></a>
+            <header>
+              <h2 className="title">{title}</h2>
+            </header>
+            <header>
+              <h3 className="subtitle">{subtitle}</h3>
+            </header>
+            <div className="iconsProj">
+              <div className="containerss">
+                <div className="wave"></div>
+              </div>
+              <div className="fonts icons">
+                <a href={githubUrl}>
+                  {' '}
+                  <motion.span
+                    className="icon fa-github"
+                    whileHover={{ color: '#e27689' }}
+                    transition={{ stype: 'spring', stiffness: 300 }}
+                  ></motion.span>
+                </a>
+                <a href={projectUrl}>
+                  <motion.span
+                    whileHover={{ color: '#e27689' }}
+                    className="icon fa-globe"
+                  ></motion.span>
+                </a>
+              </div>
+            </div>
+          </article>
 
-
-    <article className="item one h100">
-      <a href="/#" className="image fit">
-      </a>
-      <header>
-        <h2 className='title'>{title}</h2>
-      </header>
-      <header>
-        <h3 className='subtitle'>{subtitle}</h3>
-      </header>
-      <div className="iconsProj">
-        <div className="containerss">
-          <div className="wave">
-          </div>
-        </div>
-        <div className="fonts icons">
-       <a href={githubUrl}> <motion.span className="icon fa-github"
-        whileHover={{color: '#e27689'}} transition={{stype: 'spring', stiffness: 300}}
-        ></motion.span></a>
-        <a href={projectUrl}><motion.span 
-        whileHover={{color: '#e27689'}}
-        className="icon fa-globe"></motion.span>
-        </a></div>
-      </div> 
-    </article>
-
-
-    <ProjPic image={image} stack={stack} />
+          <ProjPic image={image} stack={stack} />
         </div>
       </motion.div>
-  </StyledProject >
-  )
-}
+    </StyledProject>
+  );
+};
 
-export default Project
+export default Project;
--- pages/index.js
+++ pages/index.js
@@ -1,150 +1,154 @@
-import Head from 'next/head'
-import SideBar from '../components/SideBar/index'
-import Scroll from '../components/Scroll'
-import allProjects from '../components/projects'
-import dynamic from 'next/dynamic'
-import Project from '../components/portfolio/Project'
-import Me from '../components/Me'
+import Head from 'next/head';
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import styled from 'styled-components';
+import SideBar from '../components/SideBar/index';
+import Scroll from '../components/Scroll';
+import allProjects from '../components/projects';
+import Project from '../components/portfolio/Project';
+import Me from '../components/Me';
+
+const ProjButton = styled.button`
+  margin: 0;
+  padding: 0 !important;
+  background-color: 'white' !important;
+`;
 
 export default function Home() {
- const sections = [
-  {id: 'top', name: 'intro', icon: 'fa-home' },
-  { id: 'portfolio', name: 'Portfolio', icon: 'fa-th' },
-  { id: 'about', name: 'About Me', icon: 'fa-user' },
-  { id: 'contact', name: 'Contact', icon: 'fa-envelope' },
-
-] 
-
-
-return (
+  const sections = [
+    { id: 'top', name: 'intro', icon: 'fa-home' },
+    { id: 'portfolio', name: 'Portfolio', icon: 'fa-th' },
+    { id: 'about', name: 'About Me', icon: 'fa-user' },
+    { id: 'contact', name: 'Contact', icon: 'fa-envelope' },
+  ];
+  const [projIndex, setProjIndex] = useState(0);
+  const handleProjShow = index => {
+    console.log(index);
+    console.log(allProjects[index]);
+    setProjIndex(index);
+  };
+  return (
     <>
       <SideBar sections={sections} />
-         <div id="main">
-      <section id="top" className="one dark cover">
-        <div className="container">
-        <h2 className="alt">
-      Hi! I'm <strong>Expedito</strong>
-      <br />
-      Web developer
-    </h2>
-    <p>I have made few things check it out.</p>
-          <footer>
-            <Scroll type="id" element={'portfolio'}>
-              <a href="#portfolio" className="button">
-                Show me
-              </a>
-            </Scroll>
-          </footer>
-        </div>
-      </section>
-
-      <section id="portfolio" className="two">
-        <div className="container">
-          <header>
-            <h2>Portfolio</h2>
-          </header>
-
-          <p>
-            Coding is bringing new ideas to life, to make the world better one line at a time. Here I showcase
-            my tries to make a difference;
-          </p>
-
-          <div className="row">
-            {allProjects.map((proj, index) => (
-              <Project key={proj.title} id={index} title={proj.title} githubUrl={proj.githubUrl} projectUrl={proj.projectUrl} subtitle={proj.subtitle} image={proj.image} stack={proj.stack} />
-
-            ))}
-            <div className="col-4 col-12-mobile">
-              <article className="item">
-                <a href="/#" className="image fit">
+      <div id="main">
+        <section id="top" className="one dark cover">
+          <div className="container">
+            <h2 className="alt">
+              Hi! I'm <strong>Expedito</strong>
+              <br />
+              Web developer
+            </h2>
+            <p>I have made few things check it out.</p>
+            <footer>
+              <Scroll type="id" element="portfolio">
+                <a href="#portfolio" className="button">
+                  Show me
                 </a>
-                <header>
-                  <h3>Magna Nullam</h3>
-                </header>
-              </article>
-              <article className="item">
-                <a href="/#" className="image fit">
-                </a>
-                <header>
-                  <h3>Natoque Vitae</h3>
-                </header>
-              </article>
-            </div>
-            <div className="col-4 col-12-mobile">
-              <article className="item">
-                <a href="/#" className="image fit">
-                </a>
-                <header>
-                  <h3>Dolor Penatibus</h3>
-                </header>
-              </article>
-              <article className="item">
-                <a href="/#" className="image fit">
-                </a>
-                <header>
-                  <h3>Orci Convallis</h3>
-                </header>
-              </article>
+              </Scroll>
+            </footer>
+          </div>
+        </section>
+
+        <section id="portfolio" className="two">
+          <div className="container">
+            <header>
+              <h2>Portfolio</h2>
+            </header>
+
+            <p>
+              Coding is bringing new ideas to life, to make the world better one
+              line at a time. Here I showcase my tries to make a difference;
+            </p>
+
+            <div className="row">
+              {
+                <Project
+                  key={allProjects[projIndex].title}
+                  id="current"
+                  title={allProjects[projIndex].title}
+                  githubUrl={allProjects[projIndex].githubUrl}
+                  projectUrl={allProjects[projIndex].projectUrl}
+                  subtitle={allProjects[projIndex].subtitle}
+                  image={allProjects[projIndex].image}
+                  stack={allProjects[projIndex].stack}
+                />
+              }
+              {allProjects.map((proj, index) => {
+                if (index !== projIndex) {
+                  return (
+                    <div className="col-4 col-12-mobile">
+                      <article className="item">
+                        <ProjButton
+                          type="button"
+                          className="image fit"
+                          onClick={() => handleProjShow(index)}
+                        >
+                          <header>
+                            <h3>Magna Nullam</h3>
+                          </header>
+                        </ProjButton>
+                      </article>
+                    </div>
+                  );
+                }
+              })}
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section id="about" className="three">
-        <div className="container">
-          <header>
-            <h2>About Me</h2>
-          </header>
+        <section id="about" className="three">
+          <div className="container">
+            <header>
+              <h2>About Me</h2>
+            </header>
 
-          <a href="/#" className="image featured">
-          </a>
+            <a href="/#" className="image featured"></a>
 
-          <p>
-            Developers football competition in diameter big price to layer the
-            pot. Chavez ultricies care who wants to CNN. Lobortis elementum
-            aliquet eget a den of which they do not hold it in hatred developers
-            nor the mountains of the deposit slip. The element of time, sem ante
-            ullamcorper dolor nulla quam placerat viverra environment is not
-            with our customers. Free makeup and skirt until the mouse or
-            partners or to decorate each targeted.
-          </p>
-        </div>
-      </section>
+            <p>
+              Developers football competition in diameter big price to layer the
+              pot. Chavez ultricies care who wants to CNN. Lobortis elementum
+              aliquet eget a den of which they do not hold it in hatred
+              developers nor the mountains of the deposit slip. The element of
+              time, sem ante ullamcorper dolor nulla quam placerat viverra
+              environment is not with our customers. Free makeup and skirt until
+              the mouse or partners or to decorate each targeted.
+            </p>
+          </div>
+        </section>
 
-      <section id="contact" className="four">
-        <div className="container">
-          <header>
-            <h2>Contact</h2>
-          </header>
+        <section id="contact" className="four">
+          <div className="container">
+            <header>
+              <h2>Contact</h2>
+            </header>
 
-          <p>
-            The element of time, sem ante ullamcorper dolor nulla quam placerat
-            viverra environment is not with our customers. Free makeup and skirt
-            until the mouse. Japan this innovative and ultricies carton salad
-            clinical ridiculous now passes from enhanced. Mauris pot innovative
-            care for my pain.
-          </p>
+            <p>
+              The element of time, sem ante ullamcorper dolor nulla quam
+              placerat viverra environment is not with our customers. Free
+              makeup and skirt until the mouse. Japan this innovative and
+              ultricies carton salad clinical ridiculous now passes from
+              enhanced. Mauris pot innovative care for my pain.
+            </p>
 
-          <form method="post" action="#">
-            <div className="row">
-              <div className="col-6 col-12-mobile">
-                <input type="text" name="name" placeholder="Name" />
+            <form method="post" action="#">
+              <div className="row">
+                <div className="col-6 col-12-mobile">
+                  <input type="text" name="name" placeholder="Name" />
+                </div>
+                <div className="col-6 col-12-mobile">
+                  <input type="text" name="email" placeholder="Email" />
+                </div>
+                <div className="col-12">
+                  <textarea name="message" placeholder="Message" />
+                </div>
+                <div className="col-12">
+                  <input type="submit" value="Send Message" />
+                </div>
               </div>
-              <div className="col-6 col-12-mobile">
-                <input type="text" name="email" placeholder="Email" />
-              </div>
-              <div className="col-12">
-                <textarea name="message" placeholder="Message" />
-              </div>
-              <div className="col-12">
-                <input type="submit" value="Send Message" />
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
-    </div>
+            </form>
+          </div>
+        </section>
+      </div>
     </>
-
-    )
+  );
 }
