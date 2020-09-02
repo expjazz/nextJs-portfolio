4b4d642ea025fc88a6a7b738b7ed68363edeead1
Expedito Andrade <expeditojazz@gmail.com>
2020-09-02 14:15:23 -0300

    add image to portfolio

--- components/SideBar/Top.js
+++ components/SideBar/Top.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 
 const StyledLogo = styled.div`
   position: relative;
   margin: 1.75em 1.5em 1.5em 1.5em;
   min-height: 48px;
   cursor: default;
-
   h1 {
     position: relative;
     color: #fff;
@@ -24,27 +24,37 @@ const StyledLogo = styled.div`
     line-height: 1.25em;
     margin: 0.5em 0 0 0;
   }
+  .round {
+    border-radius: 25px !important;
+  }
 
   .image {
     position: absolute;
-    left: 0;
+    border-radius: 25px;
+    img {
+      border-radius: 25px !important;
+    }
+    left: ${({ isLarge }) => (isLarge ? -10 : 0)};
     top: 0;
   }
 `;
 
-const Top = () => (
-  <StyledLogo id="logo">
-    <a href="/" className="b">
-      <>
-        <span className="image avatar48">
-          <img src="logo.png" height="128px" alt="" />
-        </span>
+const Top = () => {
+  const isLarge = useMediaQuery({ minDeviceWidth: 1320 });
+  return (
+    <StyledLogo id="logo" isLarge={isLarge}>
+      <a href="/" className="b">
+        <>
+          <span className="image avatar48">
+            <img src="logo.jpeg" height="128px" alt="logo" className="round" />
+          </span>
 
-        <h1 id="title">Expedito Andrade</h1>
-        <p>Full Stack Developer</p>
-      </>
-    </a>
-  </StyledLogo>
-);
+          <h1 id="title">Expedito Andrade</h1>
+          <p>Full Stack Developer</p>
+        </>
+      </a>
+    </StyledLogo>
+  );
+};
 
 export default Top;
