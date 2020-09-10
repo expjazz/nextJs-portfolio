29689a245329c53acb1b549745f34485add09473
Expedito Andrade <expeditojazz@gmail.com>
2020-09-10 12:21:42 -0300

    fix padding bug

--- components/portfolio/Project.js
+++ components/portfolio/Project.js
@@ -69,7 +69,7 @@ const Project = ({
     const handleScroll = () => {
       const yPos = window.scrollY;
       const cardPos = card.current.getBoundingClientRect().y;
-      setShow(yPos > cardPos - 300);
+      setShow(yPos > cardPos - 600);
     };
     window.addEventListener('scroll', handleScroll, false);
     return () => {
